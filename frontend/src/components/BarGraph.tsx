79ef0107ec74eb9bb5c2654cdf63f1c5d53af2c9
NoahPalmer1 <1410.noah@gmail.com>
2024-08-01 13:52:27 +1000

    boilerplate barchart code

---
+++ frontend/src/components/BarGraph.tsx
@@ -0,0 +1,90 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import {Bar} from 'react-chartjs-2'
+import {Chart, registerables} from 'chart.js'
+import {Chart as ChartJS} from 'chart.js'
+
+Chart.register(...registerables)
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+    },
+    title: {
+      display: false,
+    },
+    colors: {
+      enabled: true,
+    },
+
+  },
+  scales: {
+    x: {
+      display: false,
+    },
+    y: {
+      grid: {
+        display: true,
+      },
+      border: {
+        display: false,
+      },
+      ticks: {
+        display: false,
+      },
+    },
+  },
+}
+
+const graphData = Array.from({length: 40}, () =>
+  Math.floor(Math.random() * 100),
+)
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+const labels = graphData.map((_, index) => months[index % 12])
+export const data = {
+  labels,
+  datasets: [
+    {
+      backgroundColor: '#5052ff',
+      data: graphData,
+      barPercentage: 1,
+      borderRadius: 100,
+      borderSkipped: false,
+    },
+  ],
+}
+
+const BarGraph = () => {
+  const graphStyle = {
+    minHeight: '10rem',
+    maxWidth: '540px',
+    width: '100%',
+    border: '1px solid #C4C4C4',
+    borderRadius: '0.375rem',
+    padding: '1rem',
+  }
+
+  return (
+    <div style={graphStyle}>
+      <Bar data={data} options={options} />
+    </div>
+  )
+}
+
+export default BarGraph;
--- frontend/src/pages/ProductInfoPage.tsx
+++ frontend/src/pages/ProductInfoPage.tsx
@@ -1,4 +1,8 @@
 import { useLocation } from "react-router-dom";
+import BarGraph from "../components/BarGraph.tsx"
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler } from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler);
 
 export const ProductInfoPage = () => {
 
@@ -7,13 +11,13 @@ export const ProductInfoPage = () => {
     return (<h2>No give state.</h2>)
   }
   const {product} = state;
-
-  return (
-    <>
+  
+    return (
+    <>  
       <p>{product.title}</p>
       <p>{product.id}</p>
-      <a href="/dashboard">Go Back</a>
+      <a href="/dashboard">Go Back</a>    
+      <BarGraph/>
     </>
-    
   )
 }
