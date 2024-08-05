71ffca1a41d1f230da7e38c1a987eea2108e37c4
DevanshNZL <34780690+DevanshNZL@users.noreply.github.com>
2024-08-05 12:29:26 +1000

    Product info page updated, created skeleton for the page

--- frontend/src/pages/ProductInfoPage.tsx
+++ frontend/src/pages/ProductInfoPage.tsx
@@ -8,18 +8,47 @@ ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, T
 export const ProductInfoPage = () => {
 
   const {state} = useLocation();
+  // If state is not given:
   if (state === null) {
-    return (<h2>No give state.</h2>)
+    return (
+      <>
+        <h2>No give state.</h2>
+        <p><a href='/dashboard'>Dashboard</a></p>
+      </>
+    )
   }
   const {prodId} = state;
-  const productInfo = DummyData(prodId);
+  const productInfo = DummyData(prodId); // Get the product data information
   
     return (
-    <>  
-      <p>{productInfo.title}</p>
-      <p>{prodId}</p>
-      <a href="/dashboard">Go Back</a>    
-      <BarGraph/>
-    </>
+      <div className="flex h-[100vh]">
+        {/* Sidebar left */}
+        <div className="w-1/3 bg-green-100 flex flex-col items-center">
+        <span className="text-xl">{productInfo.title}</span>
+        <img
+          className="object-cover w-[60%] h-64 rounded-lg"
+          src={productInfo.img}
+        >
+        </img>
+        </div>
+
+        {/* Sidebar right */}
+        <div className="w-2/3 bg-red-200 flex flex-col items-center">
+          <span className="">Sentiment</span>
+          <div className="bg-white rounded-lg"><BarGraph /></div>
+          
+        </div>
+      </div>
+
+    // <>  
+    //   <p>{productInfo.title}</p>
+    //   <p>{prodId}</p>
+    //   <img
+    //       className="w-[60%] h-[40%] object-cover rounded-lg shadow-sm dark:shadow-gray-800"
+    //       src={productInfo.img}
+    //     ></img>
+    //   <a href="/dashboard">Go Back</a>    
+    //   <BarGraph/>
+    // </>
   )
 }
