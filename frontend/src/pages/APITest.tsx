7487ed7ef10651622750e08537b97caa15089758
DevanshNZL <34780690+DevanshNZL@users.noreply.github.com>
2024-08-05 16:36:48 +1000

    API call to Django server works (/user/product/home/) whilst user authenticated

--- frontend/src/pages/APITest.tsx
+++ frontend/src/pages/APITest.tsx
@@ -1,5 +1,7 @@
 import axios from "axios"
 import { useState } from "react"
+import api from "../api"
+import ProtectedRoute from "../components/ProtectedRoute"
 
 const APITest = () => {
 
@@ -16,10 +18,40 @@ const APITest = () => {
       console.log(error);
     }
   }
+
+  const getProduct = async () => {
+    console.log("Testing Django")
+    try {
+      const res = await api.get("/api/user/product/home/");
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const clearCache = () => {
+    console.log("Cleared the cache!");
+    localStorage.clear();
+  }
+
   return (
     <div>
       <button onClick={getQuote}>Get Quote</button>
       <p>{quote}</p>
+      <ProtectedRoute>
+        <button 
+        onClick={getProduct}
+        className="bg-gray-600 rounded-lg p-2 text-white"
+      >/api/user/product/home</button>
+      </ProtectedRoute>
+      <br></br>
+      <br></br>
+      <button
+        onClick={clearCache}
+        className="bg-gray-600 rounded-lg p-2 text-white"
+      >
+        Remove Local Cache
+      </button>
     </div>
   )
 }
