36ca2ce896bf6631f6f1c1e2c3c3e25970dea37b
LucaRosic <lucarosic@gmail.com>
2024-08-06 12:10:05 +1000

    Updated backend

--- backend/api/models.py
+++ backend/api/models.py
@@ -16,14 +16,26 @@ class Product(models.Model):
 class User_Products(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+<<<<<<< Updated upstream
     
     
+=======
+
+    def __str__(self):
+        return "%s - %s" %(self.user, self.product.name)
+
+
+# product_reviews table   
+>>>>>>> Stashed changes
 class Product_Reviews(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     review = models.CharField(max_length=8000)
     sentiment = models.DecimalField(max_digits=3, decimal_places=2)
     rating = models.DecimalField(max_digits=2, decimal_places=0)
     date = models.DateField()
+
+    def __str__(self):
+        return "%s - %s" %(self.pk,self.product.name)
  
     
 class Product_Summary(models.Model):
@@ -32,4 +44,8 @@ class Product_Summary(models.Model):
     avg_sentiment = models.DecimalField(max_digits=3, decimal_places=2)
     avg_rating = models.DecimalField(max_digits=4, decimal_places=2)
     date = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.product.name
+ 
     
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,7 +3,16 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer, ProductSerializer, UserProductSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+<<<<<<< Updated upstream
 from .models import Product, User_Products
+=======
+from .models import Product, User_Products, Product_Summary, Product_Reviews
+from scrape.scrapper import scrape_reviews
+from ML.ReviewSumModel import summarize
+from ML.sentiment import analyseSentiment, start_model
+import datetime
+
+>>>>>>> Stashed changes
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
@@ -36,8 +45,48 @@ class CreateProductView(generics.ListCreateAPIView):
             # Perfrom scraping and elt here
             # add to db 
             
+<<<<<<< Updated upstream
             # add relevant attributes
             serializer.save(name='prod', category='prod cat', description='prod descript', image='prod img')
+=======
+            # If product is already in database, only add to user_product table
+            if Product.objects.filter(url=serializer.validated_data['url']).exists():
+                user_prod = User_Products(user=User.objects.get(pk=2), product=Product.objects.get(pk=serializer.data['id']))
+                user_prod.save()
+                # add to user product table
+            else:
+                ##cleaned_data = transform_data(data)
+                # 
+                scraped = (scrape_reviews(serializer.validated_data['url']))            
+                serializer.save(name=scraped['Product Name'], category='amazon', description='prod descript', image=scraped['Product Image'])
+                
+                # adds to user_product table
+                user_prod = User_Products(user=User.objects.get(pk=2), product=Product.objects.get(pk=serializer.data['id']))
+                
+                # adds to product summary table
+                user_prod.save()
+                
+                avg_sentiment = 0
+                avg_rating = 0
+                month_dict = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
+                sent_model = start_model()
+                for review in scraped['Reviews']:
+                    
+                    date = review['Date'].split('on ')[-1].split(' ')
+                    sentiment = analyseSentiment(sent_model, review['Review Text'])
+                    rating = float(review['Stars'].split(' ')[0])
+                    avg_rating += rating
+                    avg_sentiment += sentiment
+                    
+                    prod_rev = Product_Reviews(product=Product.objects.get(pk=serializer.data['id']), review=review['Review Text'], \
+                        sentiment=sentiment, rating=rating, date=datetime.date(int(date[-1]), month_dict[date[1]], int(date[0])))
+                    prod_rev.save()
+                    
+                avg_sentiment = round(avg_sentiment/len(scraped['Reviews']),2)
+                avg_rating = round(avg_rating/len(scraped['Reviews']),2)
+                prod_sum = Product_Summary(product=Product.objects.get(pk=serializer.data['id']), summary=summarize(scraped['Reviews']), avg_sentiment=avg_sentiment, avg_rating=avg_rating)
+                prod_sum.save()
+>>>>>>> Stashed changes
         else:
             print(serializer.errors)
 
@@ -55,6 +104,101 @@ class DeleteUserProduct(generics.DestroyAPIView):
     
     permission_classes = [AllowAny]
     
+<<<<<<< Updated upstream
     #def get_queryset(self):
         #user = self.request.user
         #return User_Products.objects.filter(user=user)      
+=======
+    
+class GetProds(generics.RetrieveAPIView):
+    
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [AllowAny]
+    
+    
+class GetUProds(generics.RetrieveAPIView):
+    
+    lookup_field = 'product_id'
+    queryset = User_Products.objects.all()
+    serializer_class = UserProductSerializer
+    permission_classes = [AllowAny]
+    
+#_____________________________________________________________________________________________________________________________
+# POST Stuff
+
+## most promising so far    
+class GetProductDetails(APIView):
+    
+    permission_classes  = [AllowAny]
+    
+    def get(self, request, product_id):
+        
+        serializer = ProductSerializer(Product.objects.filter(id=product_id), many=True)
+        
+        print(serializer.data[0]['url'])
+        
+        return Response(serializer.data)
+    
+    def delete(self, request, product_id):
+
+        # delete for user table, not whole product
+        Product.objects.filter(id=product_id).delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+## GET STUFF
+
+# WORKING REQUESTS
+#__________________________________________________________________________________________________________________________
+# INFO FOR HOME
+class GetUserProduct_HomePage(generics.ListAPIView):
+    serializer_class = ProductSumSerializer_HOME
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        
+        product_ids = User_Products.objects.filter(user=self.request.user).values_list('product', flat=True)
+                
+        return Product_Summary.objects.filter(product__in=product_ids)
+    
+
+# INFO FOR DASH
+class GetReviewSent_Dash(APIView):
+    
+    permission_classes  = [AllowAny]
+    
+    def get(self, request, product_id):
+        
+        serializer = SentimentDataSerializer_Dash(Product_Reviews.objects.filter(product=product_id), many=True)
+        
+        return Response(serializer.data)
+        
+   
+class GetProduct_Meta_Dash(APIView):
+    permission_classes = [AllowAny]
+    
+    def get(self, request, product_id):
+                
+        serializer = ProductSerializer_Dash(Product.objects.filter(pk=product_id), many=True)
+        
+        return Response(serializer.data)
+
+class GetProductSum_Dash(APIView):
+    permission_classes = [AllowAny]
+    
+    def get(self, request, product_id):
+                
+        serializer = ProductSumSerializer_Dash(Product_Summary.objects.filter(product=product_id), many=True)
+        
+        return Response(serializer.data)
+    
+
+    
+   
+        
+                               
+        
+        
+>>>>>>> Stashed changes
