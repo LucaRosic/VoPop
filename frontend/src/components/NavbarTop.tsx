0467adce08b5f0c01b2dae10a61b530a05b6db2c
DevanshNZL <34780690+DevanshNZL@users.noreply.github.com>
2024-08-06 12:23:42 +1000

    Hover function of user

--- frontend/src/components/NavbarTop.tsx
+++ frontend/src/components/NavbarTop.tsx
@@ -10,7 +10,7 @@ const NavbarTop = ({ title }: Props) => {
     <nav className="flex bg-slate-800 text-white h-12 mb-4 justify-between items-center px-4 sticky top-0 z-50 shadow-md border-b-2 border-slate-400">
       <div>{title}</div>
 
-      <div className="flex gap-3 items-center">
+      <div className="flex gap-3 items-center hover:bg-slate-700 px-2 py-1 rounded-lg">
         <div>{localStorage.getItem(USERNAME) || "Hugh Mungus"}</div>
         <div className="bg-slate-400 rounded-full p-1">
           <PersonIcon></PersonIcon>
