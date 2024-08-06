f50f91fef7d447fb7583e97197f7ff494416e9d5
LucaRosic <lucarosic@gmail.com>
2024-08-06 14:30:42 +1000

    forgot to add urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,6 +7,13 @@ urlpatterns = [
     path("product/", views.CreateProductView.as_view(), name='product-list'),
     
     
-    path("user/products/", views.ListUserProduct.as_view(), name="show-user-prods"),
-    path("user/product/delete/<int:pk>/", views.DeleteUserProduct.as_view(), name='user-remove-product'),
+    #path("user/products/", views.ListUserProduct.as_view(), name="show-user-prods"),
+    #path("user/product/delete/<int:pk>/", views.DeleteUserProduct.as_view(), name='user-remove-product'),
+    
+    # Valid Urls (use in Project)
+    path("user/product/home/", views.GetUserProduct_HomePage.as_view(), name='user-product-home'),
+    path("user/product/dashboard/sentiment/<int:product_id>/", views.GetReviewSent_Dash.as_view(), name='product-dashboard-sent'),
+    path("user/product/dashboard/meta/<int:product_id>/", views.GetProduct_Meta_Dash.as_view(), name='product-dashboard-meta'),
+    path("user/product/dashboard/summ/<int:product_id>/", views.GetProductSum_Dash.as_view(), name='product-dashboard-summ'),
+    
 ]
