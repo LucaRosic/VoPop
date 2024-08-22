af1abda1ca275a4e36911b929c8d2d40b849bd3e
DevanshNZL <34780690+DevanshNZL@users.noreply.github.com>
2024-08-22 11:43:44 +1000

    Added logout button to the product card page

--- frontend/src/components/NavbarTop.tsx
+++ frontend/src/components/NavbarTop.tsx
@@ -1,6 +1,8 @@
-import { USERNAME } from "../constants";
+import { REFRESH_TOKEN, USERNAME } from "../constants";
 import PersonIcon from "@mui/icons-material/Person";
+import LogoutIcon from '@mui/icons-material/Logout';
 import AddProductBtn from "./AddProductBtn";
+import api from "../api";
 
 interface Props {
   title: string;
@@ -8,6 +10,20 @@ interface Props {
 }
 
 const NavbarTop = ({ title, urlScraperCallback }: Props) => {
+
+ const logout = async () => {
+    console.log("Logging out");
+    try {
+      const res = await api.post("/api/logout/", {refresh_token: localStorage.getItem(REFRESH_TOKEN)});
+      localStorage.clear();
+      console.log("Cleared storage!");
+      window.location.href = "/login";
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
   return (
     // <nav className="flex bg-gradient-to-r from-slate-700 to-slate-900 text-white h-12 justify-between items-center px-4 sticky top-0 z-50 border-b-2 border-slate-400">
     <nav className="bg-[rgba(19,58,63)] text-white h-14 flex justify-between items-center px-4 sticky top-0 z-50 border-b-4 border-x-0 border-gray-900">  
@@ -16,11 +32,12 @@ const NavbarTop = ({ title, urlScraperCallback }: Props) => {
         <AddProductBtn urlScraperCallback={urlScraperCallback}/>
       </div>
 
-      <div className="flex gap-3 items-center hover:bg-slate-700 px-2 py-1">
+      <div className="flex gap-3 items-center px-2 py-1">
         <div>{localStorage.getItem(USERNAME) || "Hugh Mungus"}</div>
         <div className="bg-slate-400 rounded-full p-1">
           <PersonIcon></PersonIcon>
         </div>
+        <div className="hover:bg-green-800 p-1 px-2" onClick={logout}><LogoutIcon /></div>
       </div>
     </nav>
   );
