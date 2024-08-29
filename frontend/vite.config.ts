886cffc5c4bdcbe905e38ff74acc1b2fa0307ccd
DevanshNZL <34780690+DevanshNZL@users.noreply.github.com>
2024-08-29 11:28:06 +1000

    vite.config.ts file changed in frontend for docker to work

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [react()],
-})
+ base: "/",
+ plugins: [react()],
+ preview: {
+  port: 8080,
+  strictPort: true,
+ },
+ server: {
+  port: 8080,
+  strictPort: true,
+  host: true,
+  origin: "http://0.0.0.0:8080",
+ },
+});
